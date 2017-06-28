33b5f333f02a57870c10cb05c0b8b318ee7ac1a7
James Miranda <james.miranda@travelexplorer.com.br>
2017-06-28 17:51:37 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,51 @@
+Vue.component('message', {
+
+  props: ['title', 'body'],
+
+  data() {
+    return {
+      isVisible: true
+    };
+  },
+
+  template: `
+    <div id="root" class="container" v-show="isVisible">
+      <article class="message">
+        <div class="message-header">
+          <p>{{ title }}</p>
+          <button class="delete" @click="hideModal">X</button>
+        </div>
+        <div class="message-body">
+          {{ body }}
+        </div>
+      </article>
+    </div>
+  `,
+
+  methods: {
+    hideModal() {
+        this.isVisible = false;
+    }
+  }
+});
+
+Vue.component('modal',{
+  template:`
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+          <div class="box">
+              lorem ipsum dolor sit amet
+          </div>
+        </div>
+        <button class="modal-close" @click=$emit(close)></button>
+    </div>
+  `
+});
+
+new Vue({
+  el: '#root',
+  data: {
+    showModal: false
+  }
+})
